3a422629db91379c04737704fa7bf15a2fd23757
windows <windows@MARLON>
2016-10-13 15:50:05 -0500

    actuliz

--- AzureSGADBComasur/FiltrosPersonalizados/CustomFilters.cs
+++ AzureSGADBComasur/FiltrosPersonalizados/CustomFilters.cs
@@ -12,7 +12,6 @@ namespace AzureSGADBComasur.FiltrosPersonalizados {
         {
             View = "AuthorizeFailed";
         }
-
         public string View { get; set; }
 
         /// <summary>
